7adbc56edd23653ca4a0d792a5852e2cac2e0f33
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-08 23:43:47 -0600

    Add navbar component into home page

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -1,9 +1,40 @@
+import {useState} from "react";
+import {ArrowDropDown, Notifications, Search} from "@material-ui/icons";
 import "./navbar.scss";
+import logo from "../../assets/images/Netflix_2015_logo.png";
+import avatar from "../../assets/images/user4.jpeg";
 
 const Navbar = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+	window.onscroll = () => {
+		setIsScrolled(window.pageYOffset !== 0);
+		return () => (window.onscroll = null);
+	};
 	return (
-		<div className={"navbar"}>
-			Navbar
+		<div className={isScrolled ? "navbar scrolled": "navbar"}>
+			<div className={"container"}>
+				<div className={"left"}>
+					<img src={logo} alt={"Netflix Logo"}/>
+					<span>Homepage</span>
+					<span>Series</span>
+					<span>Movies</span>
+					<span>New and Popular</span>
+					<span>My List</span>
+				</div>
+				<div className={"right"}>
+					<Search className={"icon"}/>
+					<span>KID</span>
+					<Notifications className={"icon"}/>
+					<img src={avatar} alt={"Profile Avatar"}/>
+					<div className={"profile"}>
+						<ArrowDropDown className={"icon"}/>
+						<div className={"options"}>
+							<span>Settings</span>
+							<span>Logout</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import "./home.scss";
 import {Navbar} from "../../components";
+import avatar from "../../assets/images/user1.jpeg";
 
 const Home = () => {
 	return (
 		<div className={"home"}>
+			<img width={"100%"} src={avatar} alt={"Cover"}/>
 			<Navbar/>
 		</div>
 	);
