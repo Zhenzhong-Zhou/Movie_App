447f3f2596d2bb31d4f22cd3e50154987ea246de
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-09 01:53:25 -0600

    Add lists component and animations

--- client/src/components/Lists/List/List.jsx
+++ client/src/components/Lists/List/List.jsx
@@ -1,9 +1,19 @@
 import "./list.scss";
+import cover from "../../../assets/images/user5.png";
+import {Add, PlayArrow, ThumbDownOutlined, ThumbUpOutlined} from "@material-ui/icons";
 
 const List = () => {
 	return (
 		<div className={"list"}>
-			List
+			<img src={cover} alt={"Cover"}/>
+			<div className={"listInfo"}>
+				<div className={"icons"}>
+					<PlayArrow/>
+					<Add/>
+					<ThumbUpOutlined/>
+					<ThumbDownOutlined/>
+				</div>
+			</div>
 		</div>
 	);
 };
--- client/src/components/Lists/Lists.jsx
+++ client/src/components/Lists/Lists.jsx
@@ -1,17 +1,48 @@
-import "./list.scss";
+import {useRef, useState} from "react";
 import {ArrowBackIosOutlined, ArrowForwardIosOutlined} from "@material-ui/icons";
+import "./list.scss";
 import List from "./List/List";
 
+
 const Lists = () => {
+	const [isMoved, setIsMoved] = useState(false);
+	const [slideNumber, setSlideNumber] = useState(0);
+	const listRef = useRef();
+
+	const handleClick = (direction) => {
+		setIsMoved(true);
+		let distance = listRef.current.getBoundingClientRect().x - 50;
+		if (direction === "left" && slideNumber > 0) {
+			setSlideNumber(slideNumber - 1);
+			listRef.current.style.transform = `translateX(${230 + distance}px)`;
+		}
+		if (direction === "right" && slideNumber < 5) {
+			setSlideNumber(slideNumber + 1);
+			listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+		}
+	};
+
 	return (
 		<div className={"lists"}>
 			<span className={"listsTitle"}>Continue to watch</span>
 			<div className={"wrapper"}>
-				<ArrowBackIosOutlined/>
-				<div className={"container"}>
+				<ArrowBackIosOutlined
+					className={"sliderArrow left"}
+					onClick={() => handleClick("left")}
+					style={{display: !isMoved && "none"}}/>
+				<div className={"container"} ref={listRef}>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
+					<List/>
 					<List/>
 				</div>
-				<ArrowForwardIosOutlined/>
+				<ArrowForwardIosOutlined className={"sliderArrow right"} onClick={() => handleClick("right")}/>
 			</div>
 		</div>
 	);
