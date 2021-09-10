7dfc6e28b7b02dafa8f118706037ac4a61696d03
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-10 02:41:29 -0600

    Add login route and controller and update regitser

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,33 @@
+import jwt from "jsonwebtoken";
+import CryptoJS from "crypto-js";
 import User from "../models/user.js";
 
+// REGISTER
 export const register = async (req, res) => {
 	const newUser = new User({
 		username: req.body.username,
 		email: req.body.email,
-		password: req.body.password
+		password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY)
 	});
 	try {
 		const user = await newUser.save();
 		res.status(201).json(user);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// LOGIN
+export const login = async (req, res) => {
+	try {
+		const user = await User.findOne({email: req.body.email});
+		!user && res.status(401).json("Incorrect email or password. Please try again.");
+		const decrypted = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+		const original = decrypted.toString(CryptoJS.enc.Utf8);
+		original !== req.body.password && res.status(401).json("Incorrect email or password. Please try again.");
+		const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin},
+			process.env.SECRET_KEY_TOKEN, {expiresIn: "1h"});
+		const {password, ...info} = user._doc;
+		res.status(200).json({...info, accessToken});
 	} catch (error) {
 		res.status(500).json(error);
 	}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {register} from "../controllers/auth.js";
+import {login, register} from "../controllers/auth.js";
 
 const router = express.Router();
-// REGISTER ROUTES
+// REGISTER
 router.post("/register", register);
+// LOGIN
+router.post("/login", login);
 
 export default router;
