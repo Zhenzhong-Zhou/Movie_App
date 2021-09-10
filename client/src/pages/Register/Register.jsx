1bf639890f8be6f04fd2c8195223fcec2ecaf276
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-09 23:22:36 -0600

    Add register page into app

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,45 @@
+import {useRef, useState} from "react";
 import "./register.scss";
+import logo from "../../assets/images/Netflix_2015_logo.png";
 
 const Register = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const emailRef = useRef();
+	const passwordRef = useRef();
+
+	const handleStart = () => {
+		setEmail(emailRef.current.value);
+	};
+
+	const handleFinish = () => {
+		setPassword(password.current.value);
+	};
+
 	return (
 		<div className={"register"}>
-			Register
+			<div className={"top"}>
+				<div className={"wrapper"}>
+					<img src={logo} alt={"Logo"} className={"logo"}/>
+					<button className={"login"}>Sign In</button>
+				</div>
+			</div>
+			<div className={"container"}>
+				<h1>Unlimited movies, TV shows, and more</h1>
+				<h2>Watch anywhere. Cancel anytime.</h2>
+				<p>Ready to watch? Enter your email to create or restart your membership.</p>
+				{!email ? (
+					<div className={"input"}>
+						<input type={"email"} placeholder={"Email Address"} ref={emailRef}/>'
+						<button className={"signup"} onClick={handleStart}>Get Started</button>
+					</div>
+				) : (
+					<form className={"input"}>
+						<input type={"password"} placeholder={"Password"} ref={passwordRef}/>'
+						<button className={"signup"} onClick={handleFinish}>Register</button>
+					</form>
+				)}
+			</div>
 		</div>
 	);
 };
