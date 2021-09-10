b14533b1ace27e112df063a5e83451778dad304d
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-10 03:31:48 -0600

    Add update user route and controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,9 +2,18 @@ import CryptoJS from "crypto-js";
 import User from "../models/user.js";
 
 export const update = async (req, res) => {
-	try {
-
-	} catch (error) {
-		res.status(500).json(error);
+	if (req.user.id === req.params.id || req.user.isAdmin) {
+		if (req.body.password) {
+			req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY);
+		}
+		try {
+			const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+			res.status(200).json(updatedUser);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("Cannot update this account!");
 	}
+
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
 import {update} from "../controllers/users.js";
 
 const router = express.Router();
 // UPDATE
-router.put("/:id", update);
+router.put("/:id", verifyToken, update);
 // DELETE
 // GET
 // GET USERS
