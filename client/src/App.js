5951225963c0316fb3b1c7550fa753f0f51729dd
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-11 03:24:38 -0600

    Add more link logic into app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import "./app.scss";
 import {Home, Login, Register, Watch} from "./pages";
 
 const App = () => {
+	const user = true;
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path={"/"} exact component={() => <Home/>}/>
-				<Route path={"/movies"} exact component={() => <Home type={"movies"}/>}/>
-				<Route path={"/series"} exact component={() => <Home type={"series"}/>}/>
-				<Route path={"/watch"} exact component={() => <Watch/>}/>
-				<Route path={"/register"} exact component={() => <Register/>}/>
-				<Route path={"/login"} exact component={() => <Login/>}/>
+				<Route path={"/"} exact component={() => (user ? <Home/> : <Redirect to={"/register"}/>)}/>
+				<Route path={"/register"} exact component={() => (!user ? <Register/> : <Redirect to={"/"}/>)}/>
+				<Route path={"/login"} exact component={() => (!user ? <Login/> : <Redirect to={"/"}/>)}/>
+				{user && (
+					<>
+						<Route path={"/movies"} exact component={() => <Home type={"movies"}/>}/>
+						<Route path={"/series"} exact component={() => <Home type={"series"}/>}/>
+						<Route path={"/watch"} exact component={() => <Watch/>}/>
+					</>
+				)}
 			</Switch>
 		</BrowserRouter>
 	);
