5e8880963ed2c2330fe172aeccb08f52d2c7c864
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-10 21:56:00 -0600

    Update models

--- server/models/movie.js
+++ server/models/movie.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
 		title: {type: String, required: true, unique: true},
-		description: {type: String},
+		description: {type: String, max: 500},
 		image: {type: String},
 		imageTitle: {type: String},
 		imageSmall: {type: String},
 		trailer: {type: String},
 		video: {type: String},
-		year: {type: String},
+		year: {type: String, min: 4, max: 4},
 		limit: {type: Number},
 		genre: {type: String},
 		isSeries: {type: Boolean, default: false}
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-	username: {type: String, required: true, unique: true},
-	email: {type: String, required: true, unique: true},
-	password: {type: String, required: true},
+	username: {type: String, required: true, unique: true, min: 3, max: 20},
+	email: {type: String, required: true, unique: true, max: 50},
+	password: {type: String, required: true, min: 6},
 	profile: {type: String, default: ""},
 	isAdmin: {type: Boolean, default: false}
 	},
