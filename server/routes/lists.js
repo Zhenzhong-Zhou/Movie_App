88e9d364936b6efbba819f1053186d13191bc9e4
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-10 22:11:52 -0600

    Fix bugs and performance improvement

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {verifyToken} from "../middleware/verifyToken";
-import {create} from "../controllers/lists";
+import {verifyToken} from "../middleware/verifyToken.js";
+import {create} from "../controllers/lists.js";
 
 const router = express.Router();
 // CREATE
 router.post("/",verifyToken, create);
 // DELETE
-router.delete();
+// router.delete();
 //
 
 export default router;
