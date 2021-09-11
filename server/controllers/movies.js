92f8e38caa7d078f6aff37dac966321598e632a2
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-10 20:50:23 -0600

    Add create movie controller into route

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -3,7 +3,13 @@ import Movie from "./../models/movie.js";
 // CREATE
 export const create = async (req, res) => {
 	if (req.user.isAdmin) {
-
+		const createMovie = new Movie(req.body);
+		try {
+			const newMovie = await createMovie.save();
+			res.status(201).json(newMovie);
+		} catch (error) {
+			res.status(500).json(error);
+		}
 	} else {
 		res.status(403).json("Cannot create movie!!");
 	}
