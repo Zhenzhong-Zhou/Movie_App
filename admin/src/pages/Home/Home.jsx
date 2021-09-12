c33dd5ae9e3e6b74f47340056cd38e80876d5c1b
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-11 23:58:15 -0600

    Fetch user analytics data in home page from admin

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,12 +1,39 @@
+import {useEffect, useMemo, useState} from "react";
 import "./styles.css";
 import {Chart, FeaturedInfo, WidgetLg, WidgetSm} from "../../components";
-import {userData} from "../../boilerplateData";
+import {axiosInstance} from "../../api";
 
 const Home = () => {
+	const months = useMemo(() => [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	], []);
+	const [userStatus, setUserStatus] = useState([]);
+
+	useEffect(() => {
+		const fetchStatus = async () => {
+			try {
+				const {data} = await axiosInstance.get("users/status",
+					{headers: {token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxM2IwY2QzNzFmNTRiMTYyMTNiZWU4YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMTQyMzk1NSwiZXhwIjoxNjMxNDI3NTU1fQ.sfp7bX-FSMDOKTQZzEwlHzPaETWb1dDCbhbdzMAFgRo"}});
+				const statusList = data.sort((a, b) => {return a._id - b._id});
+				statusList.map((item) => setUserStatus(prev => [
+					...prev,
+					{month: months[item._id - 1], "Total New User": item.total}
+				])
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchStatus()
+	}, [months]);
+
 	return (
 		<div className={"home"}>
 			<FeaturedInfo/>
-			<Chart data={userData} title={"User Analytics"} grid dataKey={"Active User"}/>
+			<Chart data={userStatus} title={"User Analytics"} grid dataKey={"Total New User"}/>
 			<div className={"homeWidgets"}>
 				<WidgetSm/>
 				<WidgetLg/>
