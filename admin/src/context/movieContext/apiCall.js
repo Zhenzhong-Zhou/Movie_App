fccc51af01010f4d588fdc32cb1f0171478bda02
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-12 03:21:42 -0600

    Fix bug

--- admin/src/context/movieContext/apiCall.js
+++ admin/src/context/movieContext/apiCall.js
@@ -1,12 +1,12 @@
 import {fetchMovieFailure, fetchMovieStart, fetchMovieSuccess} from "./MovieActions";
 import {axiosInstance} from "../../api";
 
-export const fetchMovie = async (dispatch) => {
+export const fetchMovies = async (dispatch) => {
 	dispatch(fetchMovieStart())
 	try {
 		const {data} = await axiosInstance.get("/movies",
 			{headers: {token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken}});
-		dispatch(fetchMovieSuccess());
+		dispatch(fetchMovieSuccess(data));
 	} catch (error) {
 		dispatch(fetchMovieFailure());
 	}
