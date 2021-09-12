270e32891e9e6ef5c4075ac7dae2c18f7fb481b5
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-12 00:12:42 -0600

    Fetch data in widgetSm component from admin

--- admin/src/components/Widget/WidgetSm/WidgetSm.jsx
+++ admin/src/components/Widget/WidgetSm/WidgetSm.jsx
@@ -1,20 +1,38 @@
-import "./styles.css";
-import user from "../../../assets/images/user.jpeg";
+import {useEffect, useState} from "react";
 import {Visibility} from "@material-ui/icons";
+import "./styles.css";
+import avatar from "../../../assets/images/avatar.png";
+import {axiosInstance} from "../../../api";
 
 const WidgetSm = () => {
+	const [newUsers, setNewUsers] = useState([]);
+
+	useEffect(() => {
+		const fetchNewUsers = async () => {
+			try {
+				const {data} = await axiosInstance.get("users?new=true",
+					{headers: {token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxM2IwY2QzNzFmNTRiMTYyMTNiZWU4YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMTQyMzk1NSwiZXhwIjoxNjMxNDI3NTU1fQ.sfp7bX-FSMDOKTQZzEwlHzPaETWb1dDCbhbdzMAFgRo"}});
+				setNewUsers(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchNewUsers();
+	}, []);
+
 	return (
 		<div className={"WidgetSm"}>
 			<span className={"WidgetSmTitle"}>New Join Members</span>
 			<ul className={"WidgetSmList"}>
-				<li className={"WidgetSmListItem"}>
-					<img src={user} alt={"Avatar"} className={"WidgetSmImg"}/>
-					<div className={"WidgetSmUser"}>
-						<span className={"WidgetSmUsername"}>Crystal Liu</span>
-						<span className={"WidgetSmUserTitle"}>Actress</span>
-					</div>
-					<button className={"WidgetSmButton"}><Visibility className={"WidgetSmIcon"}/>Display</button>
-				</li>
+				{newUsers.map((newUser) => (
+					<li className={"WidgetSmListItem"}>
+						<img src={newUser.profile || avatar} alt={"Avatar"} className={"WidgetSmImg"}/>
+						<div className={"WidgetSmUser"}>
+							<span className={"WidgetSmUsername"}>{newUser.username}</span>
+						</div>
+						<button className={"WidgetSmButton"}><Visibility className={"WidgetSmIcon"}/>Display</button>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
