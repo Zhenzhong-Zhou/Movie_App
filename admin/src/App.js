82e59ae41a87f677e96087da7d5064eac0a48ad9
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-12 02:36:19 -0600

    Add user and some logic into app, and shorten router

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,38 +1,32 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {useContext} from "react";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {NavBar, SideBar} from "./components";
 import "./App.css";
 import {Home, Login, NewProduct, NewUser, Product, ProductList, User, UserList} from "./pages";
+import {AuthContext} from "./context/authContext/AuthContext";
 
 const App = () => {
+	const {user} = useContext(AuthContext);
+
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path={"/login"} component={Login}/>
-				<NavBar/>
-				<div className={"container"}>
-					<SideBar/>
-						<Route exact path={"/"}>
-							<Home/>
-						</Route>
-						<Route exact path={"/users"}>
-							<UserList/>
-						</Route>
-						<Route exact path={"/user/:userId"}>
-							<User/>
-						</Route>
-						<Route exact path={"/newUser"}>
-							<NewUser/>
-						</Route>
-						<Route exact path={"/movies"}>
-							<ProductList/>
-						</Route>
-						<Route exact path={"/movie/:movieId"}>
-							<Product/>
-						</Route>
-						<Route exact path={"/newMovie"}>
-							<NewProduct/>
-						</Route>
-				</div>
+				<Route path={"/login"} component={() => !user ? <Login/> : <Redirect to={"/"}/>}/>
+				{user &&
+				<>
+					<NavBar/>
+					<div className={"container"}>
+						<SideBar/>
+						<Route exact path={"/"} component={Home}/>
+						<Route exact path={"/users"} component={UserList}/>
+						<Route exact path={"/user/:userId"} component={User}/>
+						<Route exact path={"/newUser"} component={NewUser}/>
+						<Route exact path={"/movies"} component={ProductList}/>
+						<Route exact path={"/movie/:movieId"} component={Product}/>
+						<Route exact path={"/newMovie"} component={NewProduct}/>
+					</div>
+				</>
+				}
 			</Switch>
 		</BrowserRouter>
 	);
